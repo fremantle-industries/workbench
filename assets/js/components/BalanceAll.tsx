0cdf9782411510a95d190f15613edd07194b405c
Gunnar R <gunnarrosenberg@gmail.com>
2020-10-01 19:01:55 -0700

    Updated table formatting and styles + theme colors

--- assets/js/components/BalanceAll.tsx
+++ assets/js/components/BalanceAll.tsx
@@ -28,9 +28,9 @@ const BalanceAll: React.FC<Props> = ({ finishTimes, usdBalances, btcBalances, bt
 };
 
 enum Color {
-  Usd = '#02c802',
-  Btc = '#ffb326',
-  BtcUsd = '#6fafff'
+  Usd = '#50ee61', //'#02c802',
+  Btc = '#ffc332', //'#ffb326',
+  BtcUsd = '#727DF5' //'#6fafff'
 }
 
 enum AxisId {
