a4b914ada0fc153eb938f625fa6002d97116692f
Alex <alex+git@fremantle.io>
2021-02-05 22:05:54 -0800

    Release v0.0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Workbench.MixProject do
   def project do
     [
       app: :workbench,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
