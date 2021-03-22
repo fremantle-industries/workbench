4c1458d79bb7dcb72806860f1dcef378f525bc76
Alex <alex+git@fremantle.io>
2021-03-21 21:06:05 -0700

    Handle monotonic time in `relative_time` util helper

--- lib/workbench_web/time.ex
+++ lib/workbench_web/time.ex
@@ -3,4 +3,10 @@ defmodule WorkbenchWeb.Time do
     relative = Timex.format!(d, "{relative}", :relative)
     Phoenix.HTML.Tag.content_tag(:span, relative, title: d)
   end
+
+  def relative_time(d) when is_integer(d) do
+    (d + System.time_offset(:microsecond))
+    |> DateTime.from_unix!(:microsecond)
+    |> relative_time()
+  end
 end
