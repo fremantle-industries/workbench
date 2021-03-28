66f0bb0e9a599ef482f2f528c63d90384a4dd70b
Alex <alex+git@fremantle.io>
2021-03-28 11:11:25 -0700

    Add node selector to accounts#index

--- lib/workbench/accounts.ex
+++ lib/workbench/accounts.ex
@@ -1,10 +1,11 @@
 defmodule Workbench.Accounts do
   @zero Decimal.new(0)
 
-  def where(filters) do
+  def where(node_param, filters) do
     show_zero = filters |> Keyword.get(:show_zero, false)
 
-    Tai.Venues.AccountStore.all()
+    [node: String.to_atom(node_param)]
+    |> Tai.Commander.accounts()
     |> Enum.filter(fn ab ->
       if show_zero do
         ab
--- lib/workbench_web/live/account_live/index.ex
+++ lib/workbench_web/live/account_live/index.ex
@@ -2,12 +2,14 @@ defmodule WorkbenchWeb.AccountLive.Index do
   use WorkbenchWeb, :live_view
 
   @default_show_zero false
-  def mount(_params, _session, socket) do
+  def mount(params, _session, socket) do
     Tai.SystemBus.subscribe(:account_store)
+    sorted_node = params |> Map.get("node", Atom.to_string(node()))
 
     socket =
       socket
-      |> assign(:accounts, accounts(show_zero: @default_show_zero))
+      |> assign(:node, sorted_node)
+      |> assign(:accounts, accounts(sorted_node, show_zero: @default_show_zero))
       |> assign(:show_zero, @default_show_zero)
 
     {:ok, socket}
@@ -19,7 +21,20 @@ defmodule WorkbenchWeb.AccountLive.Index do
     socket =
       socket
       |> assign(show_zero: show_zero)
-      |> assign(:accounts, accounts(show_zero: show_zero))
+      |> assign(:accounts, accounts(socket.assigns.node, show_zero: show_zero))
+
+    {:noreply, socket}
+  end
+
+  def handle_event("node_selected", params, socket) do
+    sorted_node = params |> Map.get("node", Atom.to_string(node()))
+    params = %{node: sorted_node}
+
+    socket =
+      socket
+      |> assign(:node, sorted_node)
+      |> assign(:accounts, accounts(sorted_node, show_zero: socket.assigns.show_zero))
+      |> push_patch(to: Routes.live_path(socket, WorkbenchWeb.AccountLive.Index, params))
 
     {:noreply, socket}
   end
@@ -27,7 +42,7 @@ defmodule WorkbenchWeb.AccountLive.Index do
   def handle_info({:account_store, :after_put, _account}, socket) do
     socket =
       socket
-      |> assign(:accounts, accounts(show_zero: socket.assigns.show_zero))
+      |> assign(:accounts, accounts(socket.assigns.node, show_zero: socket.assigns.show_zero))
 
     {:noreply, socket}
   end
@@ -37,9 +52,9 @@ defmodule WorkbenchWeb.AccountLive.Index do
   end
 
   @order ~w(venue_id asset credential_id)a
-  defp accounts(show_zero: show_zero) do
-    [show_zero: show_zero]
-    |> Workbench.Accounts.where()
+  defp accounts(node_param, [show_zero: _] = filters) do
+    node_param
+    |> Workbench.Accounts.where(filters)
     |> Enumerati.order(@order)
   end
 end
