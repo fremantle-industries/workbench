eac2b1c111c68a79e8fa2e86d27870d2d769a554
Alex <alex+git@fremantle.io>
2021-04-30 11:31:17 -0700

    Fix prod asset minimization with postcss-preset-env

--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   plugins: {
+    'postcss-preset-env': {
+      autoprefixer: {
+        flexbox: 'no-2009',
+      },
+      stage: 3,
+      features: {
+        'custom-properties': false,
+      },
+    },
     "postcss-import": {},
     tailwindcss: {},
     autoprefixer: {},
