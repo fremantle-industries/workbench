1e83aa391dac2d45bde2d051029a7108d44768e4
Alex <alex+git@fremantle.io>
2021-05-23 08:22:12 -0700

    Bump tai to v0.0.66 & add Erlang 24 to test matrix

--- mix.exs
+++ mix.exs
@@ -58,7 +58,7 @@ defmodule Workbench.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:schoolbus, "~> 0.0.3"},
       {:stored, "~> 0.0.4"},
-      {:tai, "~> 0.0.65"},
+      {:tai, "~> 0.0.66"},
       {:telemetry, "~> 0.4"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_metrics_prometheus, "~> 1.0"},
