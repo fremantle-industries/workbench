21e5994094d20d08b162567c051c058fc8b687ab
Alex <alex+git@fremantle.io>
2021-05-27 15:01:52 -0700

    Add node connect error notification

--- lib/workbench/cluster/nodes.ex
+++ lib/workbench/cluster/nodes.ex
@@ -1,14 +1,14 @@
 defmodule Workbench.Cluster.Nodes do
   def connect_node(node) do
     result = apply(:net_kernel, :connect_node, [node])
-    create_notification(:connect, node)
+    create_notification(:connect, result, node)
 
     result
   end
 
   def disconnect_node(node) do
     result = apply(:erlang, :disconnect_node, [node])
-    create_notification(:disconnect, node)
+    create_notification(:disconnect, result, node)
 
     result
   end
@@ -18,17 +18,31 @@ defmodule Workbench.Cluster.Nodes do
     result
   end
 
-  defp create_notification(:connect, node) do
+  defp create_notification(:connect, true, node) do
     subject = "Node connected"
     message = "#{node} connected to the cluster"
     tags = ["cluster", "connect"]
     Notified.create(subject, message, tags)
   end
 
-  defp create_notification(:disconnect, node) do
+  defp create_notification(:connect, false, node) do
+    subject = "Node could not connect"
+    message = "#{node} could not connect to the cluster"
+    tags = ["cluster", "connect_error"]
+    Notified.create(subject, message, tags)
+  end
+
+  defp create_notification(:disconnect, true, node) do
     subject = "Node disconnected"
     message = "#{node} disconnected from the cluster"
     tags = ["cluster", "disconnect"]
+    Notified.create(subject, message, tags)
+  end
+
+  defp create_notification(:disconnect, false, node) do
+    subject = "Node could not disconnect"
+    message = "#{node} could not disconnect from the cluster"
+    tags = ["cluster", "disconnect_error"]
     Notified.create(subject, message, tags)
   end
 end
