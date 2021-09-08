cef71cafa0076abb2f1a75898d61b061deb45916
alex <alex+git@fremantle.io>
2021-09-08 10:47:26 -0700

    Don't need pipeline to get db_prefix

--- lib/workbench/db.ex
+++ lib/workbench/db.ex
@@ -1,7 +1,6 @@
 defmodule Workbench.Db do
   @spec db_prefix :: atom | nil
   def db_prefix do
-    :workbench
-    |> Application.get_env(:db_prefix, nil)
+    Application.get_env(:workbench, :db_prefix, nil)
   end
 end
